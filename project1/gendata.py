5409f3a189600f5b1c87daabd90ee30113c5f40a
HanYu <moonlight612311521@gmail.com>
2017-10-22 10:49:26 +0800

    add project and dropout out and newpng

---
+++ project1/gendata.py
@@ -0,0 +1,8 @@
+
+
+
+def gen_data(meta, data, output):
+    pass
+
+if __name__ == "__main__":
+    gen_data("./raw_data/train/train.csv", "./raw_data/train/transferred_train/", "./data/train/")
---
+++ startpro1.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -v /Users/Moonlight/git/cs5242/project1:/workspace/project1 -m 8g --name=cs5242 bvlc/caffe:cpu bash 
